e248342a78a90a22d2741bc914e4540c01e774cf
Philippe Zimmermann <ge47qax@mytum.de>
2023-12-15 10:00:22 +0100

    Day 12 final implementation removed testing

--- Day12/main.py
+++ Day12/main.py
@@ -57,101 +57,55 @@ lines = list(zip(strings, checks))
 
 # Part 2
 
-class Posible:
-    def __init__(self, s, groups, cur_chain, found, remaining):
-        self.s = s
-        self.groups = groups
-        self.cur_chain = cur_chain
-        self.found = found
-        self.remaining = remaining
+def getLines():
+    with open("input.txt", "r") as f:
+        lines = [line.rstrip("\n") for line in f]
 
-    def addGroup(self):
-        self.groups.append(self.cur_chain)
+    strings = []
+    checks = []
+    for x in lines:
+        see = x
+        strings.append(x.split()[0])
+        checks.append(tuple([int(toint) for toint in x.split()[1].split(",")]))
 
-
-def isValid2(posibleL, checkL):
-    if len(posibleL.groups) > len(checkL):
-        return False
-    sum_check = sum(checkL)
-    if posibleL.found > sum_check:
-        return False
-    if posibleL.remaining < sum_check - posibleL.found:
-        return False
-    if not len(posibleL.groups) > len(checkL) - 1 and posibleL.cur_chain > checkL[len(posibleL.groups)]:
-        return False
-    for li, lx in enumerate(posibleL.groups):
-        if lx != checkL[li]:
-            return False
-
-    return True
+    return list(zip(strings, checks))
 
 
-def solve_part2(lline):
-    cur_str = lline[0]
+lines = getLines()
 
-    check = lline[1]
-    maxHash = cur_str.count("#") + cur_str.count("?")
-    all_possible = []
-    first = Posible("", [], 0, 0, maxHash)
-    all_possible.append(first)
-
-    for i, x in enumerate(cur_str):
-        new_all_possible = []
-        for j, y in enumerate(all_possible):
-            if x == "#":
-                y.s += "#"
-                y.found += 1
-                y.remaining -= 1
-                if i == len(cur_str) - 1:
-                    y.groups += [y.cur_chain + 1]
-                    y.cur_chain = 0
-                else:
-                    y.cur_chain += 1
-            elif x == ".":
-                y.s += "."
-                if y.cur_chain > 0:
-                    if new_all_possible:
-                        new_all_possible[j].addGroup()
-                    else:
-                        all_possible[j].addGroup()
-                    y.cur_chain = 0
-            else:
-                if y.cur_chain > 0:
-                    new_pos1 = Posible(y.s + ".", y.groups.copy() + [y.cur_chain], 0, y.found, y.remaining - 1)
-                else:
-                    new_pos1 = Posible(y.s + ".", y.groups.copy(), 0, y.found, y.remaining - 1)
-                if i == len(cur_str) - 1:
-                    new_pos2 = Posible(y.s + "#", y.groups.copy() + [y.cur_chain + 1], 0, y.found + 1, y.remaining - 1)
-                else:
-                    new_pos2 = Posible(y.s + "#", y.groups.copy(), y.cur_chain + 1, y.found + 1, y.remaining - 1)
-                if isValid2(new_pos1, check):
-                    new_all_possible.append(new_pos1)
-                if isValid2(new_pos2, check):
-                    new_all_possible.append(new_pos2)
-            if not isValid2(y, check):
-                all_possible.remove(y)
-
-        if new_all_possible:
-            all_possible = new_all_possible.copy()
-
-    print(sorted([cstr.s for cstr in all_possible]))
-
-    return len(all_possible)
-
-"""
-res2 = 0
-for line in lines:
-    solve_part2(line)
-"""
+cache = {}
+def solve(strSq, check):
+    if strSq == "":
+        return 1 if check == () else 0
+    if check == ():
+        return 0 if "#" in strSq else 1
+    key = (strSq, check)
+    if key in cache:
+        return cache[key]
+    res = 0
+    if strSq[0] in ".?":
+        res += solve(strSq[1:], check)
+    if strSq[0] in "#?":
+        if len(strSq) >= check[0] and "." not in strSq[:check[0]] \
+                and (check[0] == len(strSq) or strSq[check[0]] != "#"):
+            res += solve(strSq[check[0] + 1:], check[1:])
+    cache[key] = res
+    return res
 
 res = 0
+respart1 = 0
+for i, line in enumerate(lines):
+    respart1 += len(variations(line[0], list(line[1])))
+    str2 = ((line[0] + "?") * 5)[:-1]
+    check2 = []
+    for _ in range(5):
+        for x in line[1]:
+            check2.append(x)
+    part2_line = (str2, tuple(check2))
+    curRes = solve(part2_line[0], part2_line[1])
+    res += curRes
 
-resSave = ""
-for i, x in enumerate(lines):
-    var_save = variations(x[0], x[1])
-    resSave += str(i) + ", " + str(len(var_save)) + "\n"
-
-with open("Save.txt", "w") as f:
-    f.write(resSave)
+print(respart1)
+print(res)
 
 
